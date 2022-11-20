e24ce9d51cfcc9b802df3866b1b4db2028895acc
YvvY <momboisse.yvn@gmail.com>
2022-11-20 14:36:23 +0100

    feat/api-doc init

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,45 @@
 import express, { Application, Request, Response } from "express";
+import swaggerUI from "swagger-ui-express";
+import swaggerJsDoc from "swagger-jsdoc";
 
 const app: Application = express();
 
 const port: number = 3001;
 
-app.get("/toto", (req: Request, res: Response) => {
-  res.send("Hello toto");
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Limiter",
+      version: "1.0.0",
+      description: "A simple limiter",
+    },
+    servers: [
+      {
+        url: "http://localhost:3001",
+      },
+    ],
+  },
+  apis: ["./src/app.ts"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
+/**
+ * @openapi
+ * /health:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Responds if the app is up and running
+ *     responses:
+ *       200:
+ *         description: App is up and running
+ */
+app.get("/health", (req: Request, res: Response) => {
+  res.send("Service is up.");
 });
 
 app.listen(port, function () {
