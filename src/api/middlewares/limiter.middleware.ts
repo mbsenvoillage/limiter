db9d4e9618928c4f7cb4238f27251eb6eecaf620
YvvY <momboisse.yvn@gmail.com>
2022-11-29 21:43:52 +0100

    bugfix: catch limiter errors

--- src/api/middlewares/limiter.middleware.ts
+++ src/api/middlewares/limiter.middleware.ts
@@ -11,30 +11,35 @@ export default function limiter(
       let e = new Error("Limiter arguments must all be truthy");
       return next(e);
     }
-    let ttl: number;
-    let hits: number;
-    let ip = req.ip;
-    let auth = req.headers.authorization;
-    let path = req.path;
-    let routeWeights = config.limiter.routeWeights;
-    let currentRouteWeight = routeWeights[path as keyof typeof routeWeights];
-    let key = auth ? auth.split(" ")[1] : ip;
+    try {
+      let ttl: number;
+      let hits: number;
+      let ip = req.ip;
+      let auth = req.headers.authorization;
+      let path = req.path;
+      let routeWeights = config.limiter.routeWeights;
+      let currentRouteWeight =
+        routeWeights[path as keyof typeof routeWeights] || 1;
+      let key = auth ? auth.split(" ")[1] : ip;
 
-    const keyExists = await redis.exists(key);
-    hits = await redis.incrBy(key, currentRouteWeight);
+      const keyExists = await redis.exists(key);
+      hits = await redis.incrBy(key, currentRouteWeight);
+      if (!keyExists) {
+        await redis.expire(key, timeFrame);
+        ttl = timeFrame;
+      } else {
+        ttl = await redis.ttl(key);
+      }
 
-    if (!keyExists) {
-      await redis.expire(key, timeFrame);
-      ttl = timeFrame;
-    } else {
-      ttl = await redis.ttl(key);
+      if (hits > reqLimit) {
+        return res.status(429).json({
+          error: `You have made ${hits} request but your account is currently limited at ${reqLimit} requests. You can make a request in ${ttl} s`,
+        });
+      }
+    } catch (e) {
+      next(e);
     }
 
-    if (hits > reqLimit) {
-      return res.status(429).json({
-        error: `You have made ${hits} request but your account is currently limited at ${reqLimit} requests. You can make a request in ${ttl} s`,
-      });
-    }
     next();
   };
 }
